1dc3db09d9ac4c18f6854e1ba10ce0540d283157
alfre <alfredo.medina@uc.cl>
2023-11-14 23:00:53 -0300

    feat: select formality type

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,38 @@ import { ChatGPT, ChatGPTAzure } from "./lib/utils";
 import openai_image from "./assets/openai.png";
 import { SlowText } from "./components/SlowText";
 
-const EXAMPLE_PROMPT = `Hola,
-La verdad es que no le entendí nada a tu última clase. ¿Podrías explicar eso otra vez? Si no, me rindo.
+const EXAMPLE_PROMPT = `Hola profe,
+La verdad es que no le entendí nada a tu última clase.
+¿Podrías hacer ejemplos de como se aplica eso de la integral de la vaca otra vez en la prox clase?
+Si no, me rindo.
 Saludos, Juan Pablo
 `;
+
+// const EXAMPLE_PROMPT = `Hey teacher,
+// I didn't understand anything in your last class. Could you give examples of how to apply that cow integral again in the next class?
+// If not, I give up.
+// Greetings, Juan Pablo
+// `;
+
+const SELECT_OPTIONS = [
+  { value: "very formal", label: "Formal" },
+  { value: "colloquial", label: "Casual" },
+  {
+    value:
+      "very colloquial like gangstar in english or 'cuico zorron de chile' if it is spanish",
+    label: "Coloquial",
+  },
+];
 
 function App() {
   // const { getOpenAIResponse, loading } = ChatGPT();
   const { getOpenAIResponse, loading } = ChatGPTAzure(); // for azure api
-  const [text, setText] = useState(EXAMPLE_PROMPT);
-  const [error, setError] = useState("");
+  const [textInput, setTextInput] = useState(EXAMPLE_PROMPT);
   const [response, setResponse] = useState("");
+  const [error, setError] = useState("");
+  const [selectedOption, setSelectedOption] = useState("formal");
   const handleClick = () => {
-    getOpenAIResponse(text)
+    getOpenAIResponse(textInput, selectedOption)
       .then((res) => {
         // console.log(res)
         setError("");
@@ -24,7 +43,7 @@ function App() {
       .catch((error) => {
         console.log(error);
         setError(
-          "Ha ocurrido un error con la API de ChatGPT. Revisa la consola para más información."
+          "Ha ocurrido un error con la API de ChatGPT. Revisa la consola de tu navegador para más información."
         );
       });
   };
@@ -33,11 +52,23 @@ function App() {
     <div className="flex flex-col items-center justify-center text-center space-y-12 py-12 bg-neutral-700 text-white min-h-screen">
       <h2 className="text-2xl font-bold">Montoya GPT - G10</h2>
 
+      <select
+        className="p-3 border-2 rounded-md bg-neutral-600 border-neutral-500 cursor-pointer"
+        value={selectedOption}
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
+        {SELECT_OPTIONS.map((option, i) => (
+          <option key={i} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
       <div className="flex flex-row">
         <textarea
           className="p-3 border-2 h-64 w-[600px] rounded-md bg-neutral-600 border-neutral-500 resize-none rounded-r-none"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+          spellCheck="false"
         ></textarea>
         <button
           onClick={handleClick}
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,33 +2,36 @@ import { useState } from "react";
 import axios from "axios";
 import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
 
-const INITIAL_PROMPT = `Soy un ia que ayuda a estudiantes y profesores reescribir
-sus correos siguiendo la siguiente estructura:
-Saludo, Presentación, Exposición del caso y Requerimiento, Despedida.
-Envía un mensaje para comenzar y yo lo redactaré por ti.
-Ten en cuenta que tienes 100 tokens de escritura como máximo.
+const API_URL = import.meta.env.VITE_OPENAI_API_URL || "your-api-url";
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY || "your-api-key";
+
+const INITIAL_PROMPT = (formalityType = "formal") =>
+  `I can write a maximum of 100 tokens per message.
+I am an AI that helps students and teachers rewrite
+their emails following the following structure:
+Greeting, Presentation, Case Exposure and Requirement, Farewell.
+Send a message to start and I will write / rewrite it for you.
+The rewriting I do will be of type ${formalityType} and
+I will rewrite it in the same language that you write to me.
+I will not respond to your messages, I will only rewrite them or change the wording.
 `;
 
 export const ChatGPT = () => {
   const [loading, setLoading] = useState(false);
-  const getOpenAIResponse = async (message) => {
+  const getOpenAIResponse = async (message, formalityType) => {
     setLoading(true);
     const data = {
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: INITIAL_PROMPT },
+        { role: "system", content: INITIAL_PROMPT(formalityType) },
         { role: "user", content: message },
       ],
     };
     const headers = {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      Authorization: `Bearer ${API_KEY}`,
     };
-    const response = await axios.post(
-      import.meta.env.VITE_OPENAI_API_URL,
-      data,
-      { headers }
-    );
+    const response = await axios.post(API_URL, data, { headers });
     setLoading(false);
     return response.data.choices[0].text;
   };
@@ -38,19 +41,18 @@ export const ChatGPT = () => {
 
 export const ChatGPTAzure = () => {
   const [loading, setLoading] = useState(false);
-  const openai = new OpenAIClient(
-    import.meta.env.VITE_OPENAI_API_URL,
-    new AzureKeyCredential(import.meta.env.VITE_OPENAI_API_KEY),
-    {
-      apiVersion: "2023-07-01-preview",
-    }
-  );
-  const getOpenAIResponse = async (message) => {
+  /* getOpenAIResponse: 
+  (message: string, formalityType: "formal" | "normal" | "colloquial")
+   => Promise<string> */
+  const getOpenAIResponse = async (message, formalityType) => {
     setLoading(true);
+    const openai = new OpenAIClient(API_URL, new AzureKeyCredential(API_KEY), {
+      apiVersion: "2023-07-01-preview",
+    });
     const response = await openai.getChatCompletions(
       "gpt-35-turbo",
       [
-        { role: "system", content: INITIAL_PROMPT },
+        { role: "system", content: INITIAL_PROMPT(formalityType) },
         { role: "user", content: message },
       ],
       {
@@ -62,7 +64,6 @@ export const ChatGPTAzure = () => {
       }
     );
     setLoading(false);
-    // console.log(response.choices);
     return response.choices[0].message.content;
   };
   return { getOpenAIResponse, loading };
